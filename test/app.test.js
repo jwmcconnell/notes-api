edd2a9458e6564a20bf3c99c30afe9f09636504e
Jackmc <jackmc1122@gmail.com>
2019-07-15 17:25:59 -0700

    Add a get user route.

--- test/app.test.js
+++ test/app.test.js
@@ -2,6 +2,23 @@ const { getUser } = require('./dataHelpers');
 const request = require('supertest');
 const app = require('../lib/app');
 
+describe('GET user route', () => {
+  it('returns a user by an id', async() => {
+    const { _id } = await getUser();
+    return request(app)
+      .get(`/api/v1/users/${_id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          name: expect.any(String),
+          email: expect.any(String),
+          __v: 0
+        });
+        expect(res.status).toEqual(200);
+      });
+  });
+});
+
 describe('POST user route', () => {
   it('returns an error for an incorrect form submission', () => {
     return request(app)
--- test/seedData.js
+++ test/seedData.js
@@ -1,20 +1,26 @@
 const User = require('../lib/models/User');
+const Login = require('../lib/models/Login');
 
 const seedUsers = [
   {
     name: 'Jack',
     email: 'jack@test.com',
+    password: 'cookies'
   },
   {
     name: 'Lance',
     email: 'lance@test.com',
+    password: 'password'
   }
 ];
 
 function seedData() {
   return Promise.all(seedUsers.map(user => {
-    const { name, email } = user;
-    return User.create({ name, email });
+    const { name, email, password } = user;
+    return User.create({ name, email })
+      .then(res => {
+        Login.create({ email, hash: password, _id: res._id });
+      });
   }));
 }
 
